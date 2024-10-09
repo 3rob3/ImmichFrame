35caea6c4d005bd32f72e4bf33032b9aac939a96
JW-CH <17313367+JW-CH@users.noreply.github.com>
2024-10-09 20:13:16 +0200

    first zoom draft

--- immichFrame.Web/src/lib/components/elements/image.svelte
+++ immichFrame.Web/src/lib/components/elements/image.svelte
@@ -12,6 +12,7 @@
 
 {#key dataUrl}
 	<img
+		id="zoom"
 		transition:fade={{ duration: ($configStore.transitionDuration ?? 1) * 1000 }}
 		class="absolute h-screen max-w-full m-0 object-contain place-self-center"
 		src={dataUrl}
@@ -24,3 +25,55 @@
 		alt="data"
 	/>
 {/key}
+
+<style>
+	#zoom {
+		-webkit-animation: zoom-in 4s ease-out infinite normal;
+		animation: zoom-in 4s ease-out infinite normal;
+		-webkit-font-smoothing: antialiased;
+	}
+
+	@-webkit-keyframes zoom-in {
+		from {
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+		to {
+			-webkit-transform: scale(1.3);
+			transform: scale(1.3);
+		}
+	}
+
+	@keyframes zoom-in {
+		from {
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+		to {
+			-webkit-transform: scale(1.3);
+			transform: scale(1.3);
+		}
+	}
+
+	@-webkit-keyframes zoom-out {
+		from {
+			-webkit-transform: scale(1.3);
+			transform: scale(1.3);
+		}
+		to {
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+	}
+
+	@keyframes zoom-out {
+		from {
+			-webkit-transform: scale(1.3);
+			transform: scale(1.3);
+		}
+		to {
+			-webkit-transform: scale(1);
+			transform: scale(1);
+		}
+	}
+</style>
